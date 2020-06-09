ce6a06d1373f02c37e52bf8b2a6251b826a4c14e
jd <duverseaujohnny21@gmail.com>
2020-06-09 16:19:51 -0400

    add the add the second form for the transportation survey
    Co-authored-by: Megan Hepner <megan.hepner@gmail.com>
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("form#transportation_survey").submit(function(event) {
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function() {
+      const workTransportationMode = $(this).val();
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $("input:checkbox[name=fun-transportation]:checked").each(function () {
+      const funTransportation = $(this).val();
+      $('#fun-responses').append(funTransportation + "<br>");
+    })
+    $('#transportation_survey').hide();
+  });
+});
